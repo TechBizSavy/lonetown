3eb81166c66926fb03ec958442366365370754ff
Harsh <harshchalwadi027@gmail.com>
2025-06-20 00:11:34 +0530

    config file

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+};
+
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+initOpenNextCloudflareForDev();
+
+export default nextConfig;
